863bfed9b7f2b9eee51fe82a393e7f442d4fd1c4
amirt-microsoft <amirt@microsoft.com>
2017-12-07 15:29:22 +0200

    Removing ejs

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(cookieParser());
 // Indicate which directory static resources
 // (e.g. stylesheets) should be served from.
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 // begin listening for requests.
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
