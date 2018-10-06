827021a8091d57c1ed2f359ec331663b2d140107
Jesus Seijas <jesus.seijas@axa-groupsolutions.com>
2018-10-06 13:16:53 +0200

    Improve server.js code style.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const crypto = require('crypto');
-const express = require("express");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const rp = require("request-promise");
+const crypto = require('crypto');
+const express = require('express');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const rp = require('request-promise');
 const cookieParser = require('cookie-parser');
 
 // Initialize the web app instance,
@@ -10,52 +10,52 @@ const app = express();
 app.use(cookieParser());
 // Indicate which directory static resources
 // (e.g. stylesheets) should be served from.
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // begin listening for requests.
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("Express server listening on port " + port);
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
 
-function isUserAuthenticated(){
-    // add here the logic to verify the user is authenticated
-    return true;
+function isUserAuthenticated() {
+  // add here the logic to verify the user is authenticated
+  return true;
 }
 
-app.get('/chatBot',  function(req, res) {
-    if (!isUserAuthenticated()) {
-        res.status(403).send();
-        return
-    }
-    const options = {
-        method: 'POST',
-        uri: 'https://directline.botframework.com/v3/directline/tokens/generate',
-        headers: {
-            'Authorization': 'Bearer ' + process.env.WEBCHAT_SECRET
-        },
-        json: true
-    };
-    rp(options)
-        .then(function (parsedBody) {
-            var userid = req.query.userId || req.cookies.userid;
-            if (!userid) {
-                userid = crypto.randomBytes(4).toString('hex');
-                res.cookie("userid", userid);
-            }
-
-            var response = {};
-            response['userId'] = userid;
-            response['userName'] = req.query.userName;
-            response['connectorToken'] = parsedBody.token;
-            response['optionalAttributes'] = {age: 33};
-            if (req.query.lat && req.query.long)  {
-                response['location'] = {lat: req.query.lat, long: req.query.long};
-            }
-            const jwtToken = jwt.sign(response, process.env.APP_SECRET);
-            res.send(jwtToken);
-        })
-        .catch(function (err) {
-            res.status(err.statusCode).send();
-            console.log("failed");
-        });
+app.get('/chatBot', (req, res) => {
+  if (!isUserAuthenticated()) {
+    res.status(403).send();
+    return;
+  }
+  const options = {
+    method: 'POST',
+    uri: 'https://directline.botframework.com/v3/directline/tokens/generate',
+    headers: {
+      Authorization: `Bearer ${process.env.WEBCHAT_SECRET}`,
+    },
+    json: true,
+  };
+  rp(options)
+    .then(parsedBody => {
+      let userid = req.query.userId || req.cookies.userid;
+      if (!userid) {
+        userid = crypto.randomBytes(4).toString('hex');
+        res.cookie('userid', userid);
+      }
+      const response = {
+        userId: userid,
+        userName: req.query.userName,
+        connectorToken: parsedBody.token,
+        optionalAttributes: { age: 33 },
+      };
+      if (req.query.lat && req.query.long) {
+        response.location = { lat: req.query.lat, long: req.query.long };
+      }
+      const jwtToken = jwt.sign(response, process.env.APP_SECRET);
+      res.send(jwtToken);
+    })
+    .catch(err => {
+      res.status(err.statusCode).send();
+      console.log('failed');
+    });
 });
