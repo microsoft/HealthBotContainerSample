7f44c3e3b1b6d9b61a233e4959b11f6096181b12
Ganesh Radhakrishnan <ganrad01@gmail.com>
2020-03-08 21:44:46 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 // (e.g. stylesheets) should be served from.
 app.use(express.static(path.join(__dirname, "public")));
 // begin listening for requests.
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log("Express server listening on port " + port);
 });
