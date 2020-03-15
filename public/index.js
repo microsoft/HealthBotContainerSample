9362107590edd6fd630263bf09358a751c1c3c5e
amirt <amirt@microsoft.com>
2020-03-15 18:36:25 +0200

    Adding reference for programmatically sending activities

--- public/index.js
+++ public/index.js
@@ -32,8 +32,55 @@ function initBotConversation() {
         // userAvatarImage: '',
         userAvatarInitials: 'You'
     };
+
+    const store = window.WebChat.createStore(
+        {},
+        function(store) {
+            return function(next) {
+                return function(action) {
+                    if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+
+                        // Use the following activity to enable an authenticated end user experience
+                        /*
+                        store.dispatch({
+                            type: 'WEB_CHAT/SEND_EVENT',
+                            payload: {
+                                name: "InitAuthenticatedConversation",
+                                value: jsonWebToken
+                            }
+                        });
+                        */
+
+                        // Use the following activity to proactively invoke a bot scenario
+                        /*
+                        store.dispatch({
+                            type: 'DIRECT_LINE/POST_ACTIVITY',
+                            meta: {method: 'keyboard'},
+                            payload: {
+                                activity: {
+                                    type: "invoke",
+                                    name: "TriggerScenario",
+                                    value: {
+                                        trigger: "{scenario_id}",
+                                        args: {
+                                            myVar1: "{custom_arg_1}",
+                                            myVar2: "{custom_arg_2}"
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                        */
+                    }
+                    return next(action);
+                }
+            }
+        }
+    );
+
     const webchatOptions = {
         directLine: botConnection,
+        store: store,
         styleOptions: styleOptions,
         userID: user.id,
         username: user.name,
