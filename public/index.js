9924e23ac9f01aa707e52afa9704c8a8d1c07de9
William Wong <compulim@hotmail.com>
2020-03-16 12:28:22 -0700

    4

--- public/index.js
+++ public/index.js
@@ -31,7 +31,7 @@ function initBotConversation() {
         // botAvatarInitials: '',
         // userAvatarImage: '',
         userAvatarInitials: 'You',
-        hideSendBox: true
+        // hideSendBox: true
     };
 
     const store = window.WebChat.createStore(
