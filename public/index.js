cc6266b6a5211da1385502fb5abeede88868279f
Steve Loethen <steve@loethen.net>
2020-03-20 12:19:28 -0500

    added location print

--- public/index.js
+++ public/index.js
@@ -121,6 +121,8 @@ function initBotConversation() {
         locale: 'en'
     };
     startChat(user, webchatOptions);
+
+    sendUserLocation(botConnection, user);
 }
 
 function startChat(user, webchatOptions) {
