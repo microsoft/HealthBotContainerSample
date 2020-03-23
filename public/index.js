2e9547ff853985fac3b5e8cb9b0b1dbbc5874697
Steve Loethen <steve@loethen.net>
2020-03-23 09:41:46 -0500

    changed bot background color

--- public/index.js
+++ public/index.js
@@ -68,8 +68,10 @@ function initBotConversation() {
     });
     const styleOptions = {
         botAvatarImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cartoon_Robot.svg/512px-Cartoon_Robot.svg.png',
+        botAvatarBackgroundColor: '#1abc9c',
         //botAvatarInitials: 'Bot',
         userAvatarImage: 'https://cdn3.iconfinder.com/data/icons/cardiovascular-1/120/heart_patient-512.png',
+        userAvatarBackgroundColor: '#1abc9c',
         //userAvatarInitials: 'You'
         avatarSize: 60,
     };
