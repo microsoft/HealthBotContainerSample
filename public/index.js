67ec931bd9057c265761cabd36a2c895b58d6d37
Steve Loethen <steve@loethen.net>
2020-03-27 09:52:29 -0500

    debug

--- public/index.js
+++ public/index.js
@@ -131,7 +131,7 @@ function initBotConversation() {
         store,
         userID: user.id,
         username: user.name,
-        locale: locale2
+        locale: localfromrequest
     };
     startChat(user, webchatOptions);
 }
