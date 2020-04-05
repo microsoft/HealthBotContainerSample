651db139960ce2c53f54532a069f17c7f473e8fb
Karim Kameka <kamekk@hotmail.com>
2020-04-05 17:24:26 -0400

    added server side IP location

--- public/index.js
+++ public/index.js
@@ -142,12 +142,15 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario                            
+                            /*
                             triggeredScenario: {
-                                trigger: "covid19_assessment",
+                                trigger: "{scenario_id}",
                                 args: {
-                                    location: location
+                                    myVar1: "{custom_arg_1}",
+                                    myVar2: "{custom_arg_2}"
                                 }
-                            }                            
+                            }
+                            */                          
                         }
                     }
                 }
