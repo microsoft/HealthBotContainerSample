84e7b4bea9418218da7ff4b1522d847db612541c
amirt <amirt@microsoft.com>
2020-04-19 19:46:15 +0300

    removing test code

--- public/index.js
+++ public/index.js
@@ -105,14 +105,15 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario
-
+                            /*
                             triggeredScenario: {
-                                trigger: "demo",
+                                trigger: "{scenario_id}",
                                 args: {
                                     myVar1: "{custom_arg_1}",
                                     myVar2: "{custom_arg_2}"
                                 }
                             }
+                            */
                         }
                     }
                 }
