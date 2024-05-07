894de2484a04893f5853b3d1c089421f7b928374
phanicxunicorn <103922541+phanicxunicorn@users.noreply.github.com>
2024-05-07 12:27:02 +0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -115,16 +115,16 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario
-                            /*
+                            
                             triggeredScenario: {
-                                trigger: "{scenario_id}",
+                                trigger: "Extend_triage_with_handoff",
                                 args: {
                                     location: location,
                                     myVar1: "{custom_arg_1}",
                                     myVar2: "{custom_arg_2}"
                                 }
                             }
-                            */
+                            
                         }
                     }
                 }
